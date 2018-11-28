abd444fe72221b4e14e1c91ef6f2d0ff7efe600f
Greg Spitz <gregspitz6@gmail.com>
2018-11-28 17:37:50 -0500

    Add test to LoginComponent

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,8 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router, ActivatedRoute } from '@angular/router';
+import { By } from '@angular/platform-browser';
+
+import { MatButtonModule } from '@angular/material';
+
+import { AngularFireAuth } from 'angularfire2/auth';
 
 import { LoginComponent } from './login.component';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { Router, ActivatedRoute } from '@angular/router';
+import { SecurityService } from '../security.service';
 
 class MockAngularFireAuth {
   authState = {
@@ -22,11 +27,15 @@ class MockActivatedRoute {
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
+  let securityService: SecurityService;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
+      imports: [
+        MatButtonModule
+      ],
       providers: [
         { provide: AngularFireAuth, useClass: MockAngularFireAuth },
         { provide: Router, useClass: MockRouter },
@@ -39,10 +48,23 @@ describe('LoginComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    securityService = TestBed.get(SecurityService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // TODO: this test creates some debugcontext error that I don't understand
+  // it('should call login on security service and navigate to home on click login', () => {
+  //   fixture.whenStable().then(() => {
+  //     spyOn(securityService, 'login').and.callThrough();
+
+  //     let loginButton = fixture.debugElement.query(By.css('.login'));
+  //     loginButton.nativeElement.click();
+
+  //     expect(securityService.login).toHaveBeenCalled();
+  //   });
+  // });
 });
