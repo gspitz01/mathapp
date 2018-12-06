5798666b536c33771ad221b09e3039b8ee994a0c
Greg Spitz <gregspitz6@gmail.com>
2018-12-05 23:37:51 -0500

    Fix e2e test for welcome message

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to math-app!');
+    expect(page.getParagraphText()).toEqual('Arithmetizer');
   });
 });
