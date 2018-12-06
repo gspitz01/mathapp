99ccc05cc8c66b65a56bedd6fdeaccddd8f6c09f
Greg Spitz <gregspitz6@gmail.com>
2018-12-05 20:31:54 -0500

    Add tests for BasicResultLimitations

---
+++ src/app/basic-result-limitations.spec.ts
@@ -0,0 +1,41 @@
+import { BasicResultLimitations } from "./basic-result-limitations";
+import { BasicResult } from "./basic-result";
+
+describe('BasicResultLimitations', () => {
+
+  function expectResultToBeFalsy(limitations: BasicResultLimitations, result: BasicResult) {
+    expect(limitations.resultSatisfiesLimitations(result)).toBeFalsy();
+  }
+
+  it('should return false on resultSatisfiesLimitations() if limitations not satisfied', () => {
+    let limitations = new BasicResultLimitations(false, false);
+    // Can't be negative
+    let result = new BasicResult(-4.5);
+    expectResultToBeFalsy(limitations, result);
+
+    limitations = new BasicResultLimitations(true, false);
+    // Can't be fraction
+    result = new BasicResult(4.5);
+    expectResultToBeFalsy(limitations, result);
+
+    // Also can't be negative
+    result = new BasicResult(-4);
+    expectResultToBeFalsy(limitations, result);
+
+    limitations = new BasicResultLimitations(true, true);
+    // Can't be fraction
+    result = new BasicResult(-4.5);
+    expectResultToBeFalsy(limitations, result);
+  });
+
+  it('should return true on resultSatisfiesLimitations() if limitations satisfied', () => {
+    const limitationsPossibilites = [[false, false], [true, false], [false, true], [true, true]];
+    const satisfactoryResults = [4.5, 4, -4.5, -4];
+
+    for (let i = 0; i < limitationsPossibilites.length; i++) {
+      let limitations = new BasicResultLimitations(limitationsPossibilites[i][0], limitationsPossibilites[i][1]);
+      let result = new BasicResult(satisfactoryResults[i]);
+      expect(limitations.resultSatisfiesLimitations(result)).toBeTruthy();
+    }
+  });
+});
