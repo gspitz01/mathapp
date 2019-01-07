88a6886e5e4fc6480d704f4be83d845de202af25
Greg Spitz <gregspitz6@gmail.com>
2019-01-07 10:29:45 -0500

    Fix routes for lazy loaded modules

--- src/app/shared/routes/content.routes.ts
+++ src/app/shared/routes/content.routes.ts
@@ -3,10 +3,10 @@ import { Routes } from "@angular/router";
 export const CONTENT_ROUTES: Routes = [
   {
     path: 'basics',
-    loadChildren: '../../modules/basics/basics.module#BasicsModule'
+    loadChildren: './modules/basics/basics.module#BasicsModule'
   },
   {
     path: 'fractions',
-    loadChildren: '../../modules/fractions/fractions.module#FractionsModule'
+    loadChildren: './modules/fractions/fractions.module#FractionsModule'
   }
 ];
