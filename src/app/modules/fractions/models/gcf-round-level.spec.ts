f4f0bd3ddca3a21d585269c91685941f15c1d009
Greg Spitz <gregspitz6@gmail.com>
2019-01-21 14:30:49 -0500

    Change how GCF questions are created

--- src/app/modules/fractions/models/gcf-round-level.spec.ts
+++ src/app/modules/fractions/models/gcf-round-level.spec.ts
@@ -6,18 +6,7 @@ describe("GcfRoundLevel", () => {
   const opLimits = new BasicOperandLimitations(true, 10, true, false);
 
   it('should create OperatorQuestion', () => {
-    let roundLevel = new GcfRoundLevel("Whatevers", 15, opLimits, opLimits, null, 100);
+    let roundLevel = new GcfRoundLevel("Whatevers", 15, opLimits, opLimits);
     expect(roundLevel.createQuestion()).toEqual(jasmine.any(BasicOperatorQuestion));
-  });
-
-  it("should not produce 1 answer with 0 percent1AnswersAllowed", () => {
-    let roundLevel = new GcfRoundLevel("No Ones", 15, opLimits, opLimits, null, 0);
-    for (let i = 0; i < 100; i++) {
-      let question = roundLevel.createQuestion() as BasicOperatorQuestion;
-      if (question.getResult().value === 1) {
-        console.log("Result is 1!");
-      }
-      expect(question.getResult().value).not.toBe(1);
-    }
   });
 });
--- src/app/modules/fractions/models/gcf-round-level.ts
+++ src/app/modules/fractions/models/gcf-round-level.ts
@@ -1,63 +1,33 @@
-import { BasicRoundLevel } from "../../../shared/models/basic-round-level";
-import { BasicOperandLimitations } from "../../../shared/models/basic-operand-limitations";
-import { BasicResultLimitations } from "../../../shared/models/basic-result-limitations";
 import { GCF } from "../../../shared/models/basic-operators";
 import { OperatorQuestion } from "../../../shared/models/operator-question";
 import { BasicOperatorQuestion } from "../../../shared/models/basic-operator-question";
-import { BasicResult } from "../../../shared/models/basic-result";
 import { BasicOperator } from "../../../shared/models/basic-operator";
+import { RoundLevel } from "src/app/shared/models/round-level";
+import { BasicOperand } from "src/app/shared/models/basic-operand";
+import { BasicOperandLimitations } from "src/app/shared/models/basic-operand-limitations";
 
-export class GcfRoundLevel extends BasicRoundLevel {
-
-  private oneAnswersCreated: number;
-  private totalAnswersCreated: number;
+export class GcfRoundLevel extends RoundLevel {
 
   /**
    *
    * @param name The name of this round
    * @param questionThresholdPerSixtySeconds How many questions to get right in 60 seconds to move to next level
-   * @param operand1Limitations Limitations for the first operand
-   * @param operand2Limitations Limitations for the second operand
-   * @param resultLimitations Limitations for the result
-   * @param percent1AnswersAllowed The percentage of answers that are allowed to be 1
+   * @param baseNumberLimit Limit for the number on which to base each operand
+   * @param multiplierLimit Limit for the number by which to multiply each operand
    */
-  constructor(name: string, questionThresholdPerSixtySeconds: number, operand1Limitations: BasicOperandLimitations,
-    operand2Limitations: BasicOperandLimitations, resultLimitations: BasicResultLimitations,
-    readonly percent1AnswersAllowed: number) {
+  constructor(name: string, questionThresholdPerSixtySeconds: number, readonly baseNumberLimit: BasicOperandLimitations,
+    readonly multiplierLimit: BasicOperandLimitations) {
 
-    super(name, [GCF], questionThresholdPerSixtySeconds, operand1Limitations, operand2Limitations, resultLimitations);
-    if (percent1AnswersAllowed > 100) {
-      percent1AnswersAllowed = 100;
-    } else if (percent1AnswersAllowed < 0) {
-      percent1AnswersAllowed = 0;
-    }
-    this.oneAnswersCreated = 0;
-    this.totalAnswersCreated = 0;
+    super(name, [GCF], questionThresholdPerSixtySeconds);
   }
 
   createQuestion(): OperatorQuestion {
-    let question: BasicOperatorQuestion = null;
-    let result: BasicResult
-    do {
-      let op1 = this.operand1Limitations.createOperand();
-      let op2 = this.operand2Limitations.createOperand();
-      let operator = this.chooseOperator() as BasicOperator;
-      question = new BasicOperatorQuestion(op1, op2, operator);
-      result = question.getResult();
-    } while ((this.resultLimitations && !this.resultSatisfiesLimitations(result)) ||
-        !this.answerSatisfies1Limit(result));
-    this.totalAnswersCreated++;
-    if (result.value === 1) {
-      this.oneAnswersCreated++;
-    }
-    return question;
-  }
-
-  private answerSatisfies1Limit(result: BasicResult): boolean {
-    if (result.value === 1 &&
-      (this.oneAnswersCreated + 1) / (this.totalAnswersCreated + 1) > this.percent1AnswersAllowed / 100) {
-      return false;
-    }
-    return true;
+    let baseNumber = this.baseNumberLimit.createOperand().value;
+    let multiplier = this.multiplierLimit.createOperand().value;
+    let op1 = new BasicOperand(baseNumber * multiplier);
+    multiplier = this.multiplierLimit.createOperand().value;
+    let op2 = new BasicOperand(baseNumber * multiplier);
+    let operator = this.chooseOperator() as BasicOperator;
+    return new BasicOperatorQuestion(op1, op2, operator);
   }
 }
