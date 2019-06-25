2d531cc54efda319a6fbe2eee481440b0c15c5d8
Greg Spitz <gregspitz6@gmail.com>
2019-06-25 02:02:42 -0400

    Add test for BasicMultiplicationRoundLevel

--- src/app/core/domain/models/basics/basic-multiplication-round-level.spec.ts
+++ src/app/core/domain/models/basics/basic-multiplication-round-level.spec.ts
@@ -32,4 +32,33 @@ describe('BasicMultiplicationRoundLevel', () => {
     testFactorLimits(0, 10);
     testFactorLimits(10, 20);
   });
+
+  it('should only show factors once until all factors have been seen', () => {
+    const lowerLimit = 4;
+    const upperLimit = 6;
+    const focusNumber = 2;
+    const roundLevel = new BasicMultiplicationRoundLevel('Whatever', 25, focusNumber, lowerLimit, upperLimit);
+    const factorsSeen = [];
+
+    for (let i = lowerLimit; i <= upperLimit; i++) {
+      const question = roundLevel.createQuestion() as BasicOperatorQuestion;
+      if (question.operand1.value === focusNumber) {
+        expect(factorsSeen.includes(question.operand2.value)).toBeFalsy();
+        factorsSeen.push(question.operand2.value);
+      } else if (question.operand2.value === focusNumber) {
+        expect(factorsSeen.includes(question.operand1.value)).toBeFalsy();
+        factorsSeen.push(question.operand1.value);
+      } else {
+        console.log('should not be here');
+      }
+    }
+    const newQuestion = roundLevel.createQuestion() as BasicOperatorQuestion;
+    if (newQuestion.operand1.value === focusNumber) {
+      expect(factorsSeen.includes(newQuestion.operand2.value)).toBeTruthy();
+    } else if (newQuestion.operand2.value === focusNumber) {
+      expect(factorsSeen.includes(newQuestion.operand1.value)).toBeTruthy();
+    } else {
+      console.log('should not be here as well');
+    }
+  });
 });
