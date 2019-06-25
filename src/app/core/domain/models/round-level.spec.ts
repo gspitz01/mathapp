6077ebe65948a54603d05566a2708ff8caaa1f33
Greg Spitz <gregspitz6@gmail.com>
2019-06-25 02:15:02 -0400

    Add test for RoundLevel

--- src/app/core/domain/models/round-level.spec.ts
+++ src/app/core/domain/models/round-level.spec.ts
@@ -1,16 +1,26 @@
 import { RoundLevel } from './round-level';
 import { OperatorQuestion } from './operator-question';
 import { ADDITION } from './basics/basic-operators';
+import { Operator } from './operator';
 
 describe('RoundLevel', () => {
   class MockRoundLevel extends RoundLevel {
     createQuestion(): OperatorQuestion {
       return null;
     }
+
+    mockChooseOperator(): Operator {
+      return this.chooseOperator();
+    }
   }
 
   it('should convert name to id, changing to lowercase, removing ":", and replacing " " with "-"', () => {
     const roundLevel = new MockRoundLevel('Something: Good For You', [ADDITION], 15);
     expect(roundLevel.id).toBe('something-good-for-you');
+  });
+
+  it('should return null on chooseOperator if no operators', () => {
+    const roundLevel = new MockRoundLevel('Something', [], 15);
+    expect(roundLevel.mockChooseOperator()).toBeFalsy();
   });
 });
