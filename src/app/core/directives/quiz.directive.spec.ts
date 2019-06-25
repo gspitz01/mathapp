00298c13b3a5cb37e911afdd3665ca304d4a886c
Greg Spitz <gregspitz6@gmail.com>
2019-06-24 20:10:41 -0400

    Fix lint warnings in QuizDirectiveSpec

--- src/app/core/directives/quiz.directive.spec.ts
+++ src/app/core/directives/quiz.directive.spec.ts
@@ -2,7 +2,7 @@ import { QuizDirective } from './quiz.directive';
 
 describe('QuizDirective', () => {
   it('should create an instance', () => {
-    let spyViewContainerRef = jasmine.createSpyObj("ViewContainerRef", ["clear", "createComponent"]);
+    const spyViewContainerRef = jasmine.createSpyObj('ViewContainerRef', ['clear', 'createComponent']);
     const directive = new QuizDirective(spyViewContainerRef);
     expect(directive).toBeTruthy();
   });
