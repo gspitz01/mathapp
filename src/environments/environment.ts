a4f4764eefe3636b6d42bb6e97a3d299897b1d25
Greg Spitz <gregspitz6@gmail.com>
2019-07-03 00:06:13 -0400

    Fix lint warnings in environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAkx1oSmye3UCwHG0D_7MqdsbxNI0swXek",
-    authDomain: "math-app-2c984.firebaseapp.com",
-    databaseURL: "https://math-app-2c984.firebaseio.com",
-    projectId: "math-app-2c984",
-    storageBucket: "math-app-2c984.appspot.com",
-    messagingSenderId: "82507325483"
+    apiKey: 'AIzaSyAkx1oSmye3UCwHG0D_7MqdsbxNI0swXek',
+    authDomain: 'math-app-2c984.firebaseapp.com',
+    databaseURL: 'https://math-app-2c984.firebaseio.com',
+    projectId: 'math-app-2c984',
+    storageBucket: 'math-app-2c984.appspot.com',
+    messagingSenderId: '82507325483'
   }
 };
 
