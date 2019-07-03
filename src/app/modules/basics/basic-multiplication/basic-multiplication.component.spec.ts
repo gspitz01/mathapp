fee03e5353a29e418976f724ef244dede99e48b5
Greg Spitz <gregspitz6@gmail.com>
2019-07-03 08:25:00 -0400

    Add tests for BasicMultiplicationComponent

--- src/app/modules/basics/basic-multiplication/basic-multiplication.component.spec.ts
+++ src/app/modules/basics/basic-multiplication/basic-multiplication.component.spec.ts
@@ -5,7 +5,7 @@ import { By } from '@angular/platform-browser';
 
 import { MatListModule } from '@angular/material';
 
-import { of } from 'rxjs';
+import { of, BehaviorSubject } from 'rxjs';
 
 import { BasicMultiplicationComponent } from './basic-multiplication.component';
 import { BasicQuizViewComponent } from '../../../shared/components/basic-quiz-view/basic-quiz-view.component';
@@ -15,13 +15,14 @@ import { MULTIPLICATION } from 'src/app/core/domain/models/basics/basic-operator
 import { BASIC_MULTIPLICATION_LEVEL_ORDER } from 'src/app/core/domain/models/basics/basic-multiplication-round-levels';
 import { ActivatedRoute } from '@angular/router';
 
-describe('BasicMultiplicationComponent', () => {
+fdescribe('BasicMultiplicationComponent', () => {
   let component: BasicMultiplicationComponent;
   let fixture: ComponentFixture<BasicMultiplicationComponent>;
   let startButton: DebugElement;
-  const activatedRoute = {roundName: 'twos'};
+  const activatedRoute = {roundName: 'threes'};
+  const activatedRouteSubject = new BehaviorSubject<any>(activatedRoute);
   const mockActivatedRoute = {
-    params: of(activatedRoute)
+    params: activatedRouteSubject
   };
 
   beforeEach(async(() => {
@@ -54,20 +55,30 @@ describe('BasicMultiplicationComponent', () => {
   });
 
   it('level order should be BASIC MULTIPLICATION', () => {
-    expect(component.levelOrder).toBe(BASIC_MULTIPLICATION_LEVEL_ORDER[0]);
+    expect(component.levelOrder).toBe(BASIC_MULTIPLICATION_LEVEL_ORDER[1]);
   });
 
-  it('quiz name should be basic-multiplication-twos', () => {
-    expect(component.quizName).toBe('basic-multiplication-twos');
+  it('quiz name should be basic-multiplication-threes', () => {
+    expect(component.quizName).toBe('basic-multiplication-threes');
   });
 
   it('should display start button', () => {
     expect(startButton.nativeElement.textContent).toBe('Start');
   });
 
-  it('should display "Level: Twos"', () => {
+  it('should display "Level: Threes"', () => {
     const levelDisplay = fixture.debugElement.query(By.css('.level'));
-    expect(levelDisplay.nativeElement.textContent).toContain('Twos');
+    expect(levelDisplay.nativeElement.textContent).toContain('Threes');
+  });
+
+  fit('should display "Level: Twos" if roundName route param not there', () => {
+    activatedRouteSubject.next({});
+    fixture = TestBed.createComponent(BasicMultiplicationComponent);
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const levelDisplay = fixture.debugElement.query(By.css('.level'));
+      expect(levelDisplay.nativeElement.textContent).toContain('Twos');
+    });
   });
 
   it('after start clicked, should display addition operator', () => {
@@ -92,7 +103,7 @@ describe('BasicMultiplicationComponent', () => {
     fixture.whenStable().then(() => {
       fixture.debugElement.query(By.css('.jump-to-level-button')).nativeElement.click();
       fixture.detectChanges();
-      const easyFivesLevelButton = fixture.debugElement.query(By.css('#hard-multiplication-twos'));
+      const easyFivesLevelButton = fixture.debugElement.query(By.css('#hard-multiplication-threes'));
       easyFivesLevelButton.nativeElement.click();
       fixture.detectChanges();
 
