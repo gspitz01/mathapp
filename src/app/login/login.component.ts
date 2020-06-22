dfc071d3550c6f61c0c540e7dc284b91df0a921a
Greg Spitz <gregspitz6@gmail.com>
2020-06-22 09:14:12 -0400

    Add error message to login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -25,6 +25,12 @@ export class LoginComponent implements OnInit {
       this.zone.run(() => {
         this.router.navigateByUrl(this.return);
       });
+    })
+    .catch((error) => {
+      console.log('Error code: ' + error.code);
+      console.log('Error message: ' + error.message);
+      console.log('Email: ' + error.email);
+      console.log('Credential: ' + error.credential);
     });
   }
 
